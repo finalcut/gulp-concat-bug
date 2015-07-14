61bb911c383891d67481d2bdcc313416bbf3867a
Bill Rawlinson <bill.rawlinson@sbcs.com>
2015-07-14 13:27:40 -0400

    updated tests to show that problem lies with spearator character

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -24,7 +24,7 @@ var paths = {
 
   test1: {
     concat: {
-      sources: [ // order of these files is important as some functions depend on others.
+      sources: [
         '../outerdata/test1/test1_UTF16LE.txt',
         '../outerdata/test1/test2_UTF16BE.txt',
         '../outerdata/test1/test3_UTF16LE.txt',
@@ -37,7 +37,7 @@ var paths = {
 
   test2: {
     concat: {
-      sources: [ // order of these files is important as some functions depend on others.
+      sources: [
         '../outerdata/test2/test1_UTF16LE.txt',
         '../outerdata/test2/test2_UTF16LE.txt',
         '../outerdata/test2/test3_UTF16LE.txt',
@@ -49,7 +49,7 @@ var paths = {
   },
   test3: {
     concat: {
-      sources: [ // order of these files is important as some functions depend on others.
+      sources: [
         '../outerdata/test3/test1_UTF16BE.txt',
         '../outerdata/test3/test2_UTF16BE.txt',
         '../outerdata/test3/test3_UTF16BE.txt',
@@ -62,7 +62,7 @@ var paths = {
 
   test4: {
     concat: {
-      sources: [ // order of these files is important as some functions depend on others.
+      sources: [
         './innerdata/test4/test1_UTF16LE.txt',
         './innerdata/test4/test2_UTF16BE.txt',
         './innerdata/test4/test3_UTF16LE.txt',
@@ -75,7 +75,7 @@ var paths = {
 
   test5: {
     concat: {
-      sources: [ // order of these files is important as some functions depend on others.
+      sources: [
         './innerdata/test5/test1_UTF8.txt',
         './innerdata/test5/test2_UTF8.txt',
         './innerdata/test5/test3_UTF8.txt',
@@ -100,9 +100,9 @@ function printToConsole(chunk){
 }
 
 
-function runConcatTest(d){
+function runConcatTest(d, newLine){ // allow override of line separator
   var testResults =  gulp.src(d.sources)
-    .pipe(concat(d.outfile, { newLine: ';' }))
+    .pipe(concat(d.outfile, { newLine: newLine }))
     .pipe(gulp.dest(d.outpath));
     testResults.on('data', printToConsole);
 }
@@ -115,32 +115,56 @@ function runNonConcatTest(d){
 
 
 gulp.task('test1', function(cb) {
+  /*
+    results in munged output because of overlap of two utf types
+    plus utf 8 line separator that is used by default
+   */
   runConcatTest(paths.test1.concat);
-  runNonConcatTest(paths.test1.concat);
+  //runNonConcatTest(paths.test1.concat);
   cb();
 });
 
 gulp.task('test2', function(cb) {
-  runConcatTest(paths.test2.concat);
-  runNonConcatTest(paths.test2.concat);
+  /*
+     concat uses a UTF8 line separator; so override it as an empty string
+     to show it works.
+
+  */
+  runConcatTest(paths.test2.concat,'');
+  //runNonConcatTest(paths.test2.concat);
   cb();
 });
 
 gulp.task('test3', function(cb) {
+  /*
+    all input files are same UTF 16 BE encoded but because of line
+    separator in gulp-utils (used by concat) there is overlap
+    and every other line is messed up.
+  */
   runConcatTest(paths.test3.concat);
-  runNonConcatTest(paths.test3.concat);
+  //runNonConcatTest(paths.test3.concat);
   cb();
 });
 
 gulp.task('test4', function(cb) {
+  /*
+    results in munged output because of overlap of two utf types
+    plus utf 8 line separator that is used by default.
+    This test was initially added to make sure relative path
+    direction wasn't the culprit for some reason.
+   */
   runConcatTest(paths.test4.concat);
-  runNonConcatTest(paths.test4.concat);
+  //runNonConcatTest(paths.test4.concat);
   cb();
 });
 
 gulp.task('test5', function(cb) {
+  /*
+    results in perfect output where each input is separated by a newline
+    because all inputs and the newline character are all UTF-8
+   */
   runConcatTest(paths.test5.concat);
-  runNonConcatTest(paths.test5.concat);
+  //runNonConcatTest(paths.test5.concat);
   cb();
 });
 
